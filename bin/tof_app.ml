5e5cd38d3b088263718dd1bbbd2588e77656b020
Thomas Haessle <thaessle@MacBook-Pro-de-Thomas.local>
2021-10-29 11:08:39 +0200

    remove pesy

--- bin/tof_app.ml
+++ bin/tof_app.ml
@@ -1,1 +1,26 @@
-let () = print_endline "hello from Troll Of Fame"
+let () = print_endline 
+" _____________________________________
+/ \\                                   \\
+|   | The King of the Trolls Gnonpom   |
+ \\_ | coded the TROLL OF FAME          |
+    | a wonderfull application that    |
+    | would help Trolls to learn       |
+    | numbers when they are hunting.   |
+    |                                  |
+    | Gnonpom was a hard skilled TDD 👑|
+    | who just released ToF when       | 
+    | all tests passed Green.          | 
+    |                                  | 
+    | Sadly he was shooted by          |
+    | a disgusting Elf.                | 
+    |                                  | 
+    | Here come a new King, Hurrah     |
+    | for the great Troll Aklass!      | 
+    |                                  | 
+    | This time it's decided,          |
+    | the elf hunting contest          |
+    | is launched!                     | 
+    |    ______________________________|_
+    |   /                               /
+     \\_/_______________________________/
+"
